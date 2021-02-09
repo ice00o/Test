3c4657e1f6e7f30a38d9f2239202f6deaceccbc6
徐少博 <xsb644805231@hotmail.com>
2021-02-09 11:49:08 +0800

    20200206添加项目文件。

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,6 +7,25 @@ namespace Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Person P1 = new Person();
+            P1.Age = 18;
+            Person P2 = new Person();
+            P2.Age = 19;
+            if(P1.CompareTo(P2) > 0 )
+            {
+                Console.WriteLine("P1比P2大");
+            }
+            else if(P1.CompareTo(P2) < 0)
+            {
+                Console.WriteLine("P1比P2小");
+            }
+            else
+            {
+                Console.WriteLine("P1和P2一样大");
+            }
+            Console.Read();
         }
+
     }
 }
+
